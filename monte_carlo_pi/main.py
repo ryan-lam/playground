2d97311a19c0f7370ed76403fc44a51665bec68c
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-08-20 20:25:48 -0400

    Add multiprocessing

--- monte_carlo_pi/main.py
+++ monte_carlo_pi/main.py
@@ -1,10 +1,14 @@
-from random import random
+import multiprocessing
 from math import sqrt
-import time
+from random import random
+from time import time
+
+import numpy as np
 
 
-def monte_carlo_pi(n=100):
-    # initialize area
+def monte_carlo_pi():
+    # initialize area, n
+    n = 1000000
     inside, outside = 0, 0
     # Simulate area
     for _ in range(n):
@@ -20,4 +24,11 @@ def monte_carlo_pi(n=100):
 
 
 if __name__ == "__main__":
-    pass
+    t1 = time()
+    with multiprocessing.Pool() as pool:
+        print(f"Using {pool._processes} processes...")
+        future_results = [pool.apply_async(monte_carlo_pi) for _ in range(1000)]
+        results = [f.get() for f in future_results]
+    t2 = time()
+    print(f"Pi approximation is {np.average(results)}")
+    print(f"Completed in {t2-t1}s")
